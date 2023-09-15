10b4f50b384b9d5e72145766df96264fc5311fec
Alex <alopezbelles@gmail.com>
2023-09-15 18:54:58 +0200

    work on styles lyout

--- src/Containers/Home/Home.jsx
+++ src/Containers/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./Home.css";
 
@@ -20,7 +20,7 @@ function Home() {
           // a must be equal to b
           return 0;
         });
-        console.log(data)
+        console.log(data);
         setPokemons(orderedList);
       });
   }, []);
@@ -29,8 +29,8 @@ function Home() {
     setSearchTerm(event.target.value);
   };
 
-  const getImageUrl = (pokemonId) => `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
-
+  const getImageUrl = (pokemonId) =>
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`;
 
   const filteredPokemons = pokemons.filter((pokemon) =>
     pokemon.name.toLowerCase().startsWith(searchTerm.toLowerCase())
@@ -38,22 +38,22 @@ function Home() {
 
   return (
     <div>
-        <div className="header">
-      <h1>¡ POKEMON FINDER !</h1>
-      <input
-        type="text"
-        className="imputDesign"
-        value={searchTerm}
-        onChange={handleChange}
-        placeholder="Find your Pokemon..."
-      />
+      <div className="header">
+        <h1>¡ POKEMON FINDER !</h1>
+        <input
+          type="text"
+          className="imputDesign"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Find your Pokemon..."
+        />
       </div>
       <div className="homePageDesign">
         {filteredPokemons.map((value) => (
           <div className="pokemonCard" key={value.name}>
             <h3>{value.name}</h3>
             <img
-              src={getImageUrl(value.url.split('/').reverse()[1])}
+              src={getImageUrl(value.url.split("/").reverse()[1])}
               alt={`Imagen de ${value.name}`}
             />
           </div>
